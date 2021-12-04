a06dad5c9b7f5f1d99ac57b4d6b5a3b3c3885832
Leak27 <oki271002@gmail.com>
2021-12-04 17:46:19 +0800

    import opencv

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print ("Hello World")
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+
+while True :
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Face Detection', frame)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
